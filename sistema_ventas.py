da45a8a1d14884b26e2556ca45dab56f4d50625c
felipe2332234-depuración <felipe2332234@gmail.com>
2026-06-27 22:33:33 -0400

    Actividad 9: Entrada de datos

---
+++ sistema_ventas.py
@@ -0,0 +1,3 @@
+cliente = input("Ingrese el nombre del cliente: ")
+cantidad_libros = int(input("Ingrese la cantidad de libros comprados: "))
+precio_unitario = float(input("Ingrese el precio unitario del libro: "))
